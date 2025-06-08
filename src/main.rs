7c3d526a990f32f35124b394c28d7c2f23c9fdd5
LitFill <marrazzy54@gmail.com>
2025-06-08 15:36:02 +0700

    init: Gyrum, scaffold a Mercury project

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::fs::{ self, File };
+use std::io::{ self, Write };
+use std::path::Path;
+use clap::{ Parser, Subcommand };
+
+#[derive(Parser, Debug)]
+#[command(about = "Scaffold a Mercury project.")]
+#[command(version, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// create new project
+    New {
+        /// name of project
+        project_name: String,
+    },
+}
+
+fn get_output(module_name: &str) -> String {
+    format!(
+":- module {}.
+
+:- interface.
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+main(!IO) :-
+    io.write_string(\"Hello, Mercury!\", !IO).
+", module_name)
+}
+
+fn scaffold_file(project_name: &str) -> io::Result<()> {
+    let dir = Path::new(project_name);
+    fs::create_dir_all(dir)?;
+    println!("created {:?}.", dir);
+
+    let fname = format!("{}.m", project_name);
+    let fname = dir.join(fname);
+    let mut file = File::create(&fname)?;
+
+    let output = get_output(project_name);
+    file.write_all(output.as_bytes())?;
+    println!("created {:?}.", fname);
+
+    Ok(())
+}
+
+fn run() -> io::Result<()> {
+    let cli = Cli::parse();
+    match cli.command {
+        Command::New{ project_name } => {
+            scaffold_file(&project_name)?;
+            Ok(())
+        }
+    }
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("[ERROR] {}", e);
+        std::process::exit(1);
+    }
+}
